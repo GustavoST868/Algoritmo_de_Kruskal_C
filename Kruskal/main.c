a9054bd4c0b6579138facc77d10d1ea3cde28432
Fulano de Tal <gustavosantosteixeira98@gmail.com>
2024-03-18 13:39:57 -0300

    2

---
+++ Kruskal/main.c
@@ -0,0 +1,38 @@
+//used libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    
+}Edge;
+
+typedef struct{
+    
+}Graph;
+
+Graph *createGraph(){
+    
+}
+
+int found(){
+    
+}
+
+void join(){
+    
+}
+
+int compare(){
+    
+}
+
+void Kruskal(){
+    
+}
+
+int main()
+{
+    
+
+    return 0;
+}
