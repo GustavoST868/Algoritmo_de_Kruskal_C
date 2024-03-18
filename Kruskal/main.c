fe54c9f0697a8ca46c5e11169b448523d1d1bab4
Gustavo Santos Teixeira <136706241+GustavoST868@users.noreply.github.com>
2024-03-18 19:43:57 -0300

    Update main.c

--- Kruskal/main.c
+++ Kruskal/main.c
@@ -2,30 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//edge definition structure
 typedef struct{
     
 }Edge;
 
+
+//graph definition structure
 typedef struct{
     
 }Graph;
 
+//function to create the graph
 Graph *createGraph(){
     
 }
 
+//function to find
 int found(){
     
 }
 
+//function to join
 void join(){
     
 }
 
+//function to compare
 int compare(){
     
 }
 
+//function for Kruskal algorithm
 void Kruskal(){
     
 }
